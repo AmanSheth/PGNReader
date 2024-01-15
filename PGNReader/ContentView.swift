b881bde95949fa3863bed96adf82af984902b6e6
Aman Sheth <amsheth2@gmail.com>
2024-01-15 11:05:23 +0530

    Initial Commit

---
+++ PGNReader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PGNReader
+//
+//  Created by Aman Sheth on 1/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("TEST 1!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PGNReader/PGNReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PGNReaderApp.swift
+//  PGNReader
+//
+//  Created by Aman Sheth on 1/5/24.
+//
+
+import SwiftUI
+
+@main
+struct PGNReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
